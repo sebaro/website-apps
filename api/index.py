2f92e1945ccbe669fa7646cacd038bd5534d389a
sebaro <esebaro@gmail.com>
2024-02-23 16:13:44 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -91,8 +91,8 @@ class handler(BaseHTTPRequestHandler):
         #self.wfile.write(str(kv.set(key="sss", value="asasd")).encode('utf-8'))
         #self.wfile.write('<br>'.encode('utf-8'))
         #self.wfile.write(str(kv.get("sss")).encode('utf-8'))    
-        self.wfile.write(str(os.getenv("KV_URL")).encode('utf-8'))     
-        r = redis.from_url(os.getenv("KV_URL"))
+        self.wfile.write(str(os.getenv("KV_URL")).encode('utf-8').replace('redis://', 'rediss://'))     
+        r = redis.from_url(os.getenv("KV_URL").replace('redis://', 'rediss://'))
         keys = r.keys()
         for key in keys:
             print('Key:', key)
