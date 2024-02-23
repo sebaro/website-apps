774c59294853d77d1721947dc8ab35e0b2b053bd
sebaro <esebaro@gmail.com>
2024-02-23 11:52:48 +0000

    Update file index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
 import pytest
 
 from vercel_kv import KV
+app = KV()
 
 @pytest.mark.asyncio
 async def test():
-    app = KV()
     print(app.has_auth())
     print(app.set(key="sss", value="asasd"))
     print(app.get("sss"))
