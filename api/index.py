2d114358f131a09724944b9f8651dce1aa72053c
sebaro <esebaro@gmail.com>
2024-02-23 10:59:00 +0000

    Update file index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,4 @@
-from http.server import BaseHTTPRequestHandler
+import redis
 
-class handler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+url_connection = redis.from_url("redis://default:3c47bc17a9d848babf5a2f90c33e4ac7@helped-macaw-35286.upstash.io:35286")
+url_connection.ping()
