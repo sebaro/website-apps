0e94b6b4cd4f9a0e9cc18453355b2d66990e7998
sebaro <esebaro@gmail.com>
2024-02-23 15:28:11 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,15 @@
-import pytest
-
 from vercel_kv import KV
-app = KV()
 
-@pytest.mark.asyncio
-async def test():
-    print(app.has_auth())
-    print(app.set(key="sss", value="asasd"))
-    print(app.get("sss"))
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write(KV.has_auth())
+        self.wfile.write(KV.set(key="sss", value="asasd"))
+        self.wfile.write(VK.get("sss"))        
+        return
