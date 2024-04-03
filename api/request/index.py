413d03b8db5e303b8f49a008b5e81fe1c1fe9d62
sebaro <esebaro@gmail.com>
2024-04-03 13:29:14 +0300

    update request api

--- api/request/index.py
+++ api/request/index.py
@@ -16,13 +16,13 @@ class handler(BaseHTTPRequestHandler):
         j = json.loads(post_data)
         url = j['url']
         if 'data' in j:
-        data = json.dumps(j['data']).encode('utf-8')
+            data = json.dumps(j['data']).encode('utf-8')
         else:
-        data = None
+            data = None
         if 'headers' in j:
-        headers = j['headers']
+            headers = j['headers']
         else:
-        headers = {'User-Agent':'curl/8.5.0'}
+            headers = {'User-Agent':'curl/8.5.0'}
         req = Request(url, data, headers)
         self.set_headers(urlopen(req).info()["content-type"])
         self.wfile.write(urlopen(req).read())
