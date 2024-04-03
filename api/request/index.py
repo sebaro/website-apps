ebee04a9f421c3d9ed13620d5f9b5d968ee6b6fc
sebaro <esebaro@gmail.com>
2024-04-03 13:38:42 +0300

    update request api

--- api/request/index.py
+++ api/request/index.py
@@ -11,6 +11,10 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type', type)
         self.end_headers()
 
+    def do_OPTIONS(self):
+        self.send_response(200)
+        return
+
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
